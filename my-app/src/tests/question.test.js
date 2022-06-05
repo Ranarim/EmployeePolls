9a278bafff85625dafffc96bf516ce00512fae5b
Johannes <74767916+Ranarim@users.noreply.github.com>
2022-06-05 22:41:22 +0200

    changing my question.test.js test

--- my-app/src/tests/question.test.js
+++ my-app/src/tests/question.test.js
@@ -1,25 +1,37 @@
-import React from "react";
-import "@testing-library/jest-dom/extend-expect";
-import { render, screen } from '../utils/test-utils';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
 import Question from "../components/Question";
-import { Provider } from "react-redux";
-import {store} from "../store";
+import {screen } from "@testing-library/react";
 
- describe("Question", () => {
-    it("Checking if the question card has all node elements", async() => {
-      render(
-            <Provider store={store}>
-                <Router>
-                    <Question />
-                </Router>
-            </Provider>
-        )
-    
-       const answered = screen.getByText(/answered polls/i);
+
+const mockStore = configureStore([]);
+
+describe('My Connected React-Redux Component', () => {
+  let store;
+  let component;
+
+  beforeEach(() => {
+    store = mockStore({
+      myState: 'sample text',
+    });
+  });
+
+  component = renderer.create(
+    <Provider store={store}>
+      <Question />
+    </Provider>
+  );
+
+  it('should render with given state from Redux store', () => {
+    expect(component.toJSON()).toMatchSnapshot();
+
+    const answered = screen.getByText(/answered polls/i);
        const unanswered = screen.getByText(/unanswered polls/i);
         expect(answered).toBeInTheDocument();
         expect(unanswered).toBeInTheDocument();
-    })
-}) 
+  });
+});
 
+ 
